2de18e1aefe93914a0012eaf72684e7fd4229122
Paul Everitt <pauleveritt@me.com>
2017-11-29 15:44:33 -0500

    Let's get the run_livereload up to date.

--- run_livereload.py
+++ run_livereload.py
@@ -1,10 +1,30 @@
+"""
+
+Automatically re-build Sphinx docs while editing, serve docs on an
+HTTP port, and also reload any browsers pointed to the docs.
+
+"""
+
+import glob
+
 from livereload import Server, shell
+from livereload.watcher import Watcher
 
 sphinx = ".venv/bin/python3 .venv/bin/sphinx-build -E -b html docs docs/_build"
 
-if __name__ == '__main__':
-    server = Server()
-    server.watch('docs/conf.py', shell(sphinx))
-    server.watch('docs/*.rst', shell(sphinx))
-    server.watch('docs/*/*.rst', shell(sphinx))
-    server.serve(root='docs/_build', live_css=False)
+
+class CustomWatcher(Watcher):
+    """ Handle recursive globs with Python 3.5+ globbing  """
+
+    def is_glob_changed(self, path, ignore=None):
+        for f in glob.glob(path, recursive=True):
+            if self.is_file_changed(f, ignore):
+                return True
+        return False
+
+
+server = Server(watcher=CustomWatcher())
+server.watch('docs/**', shell(sphinx),
+             ignore=lambda s: '_build' in s)
+server.watch('src/kaybee/**.py', shell(sphinx))
+server.serve(root='docs/_build', live_css=False)
